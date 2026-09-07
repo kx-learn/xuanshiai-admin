b73f40d9cc77b88a4ff792307d442c07d0c64515
ysc <ysc025316@qq.com>
2026-09-07 14:48:00 +0800

    1

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = { reactStrictMode: true, poweredByHeader: false, output: "standalone" };
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  output: "standalone",
+  allowedDevOrigins: ["127.0.0.1"],
+};
 export default nextConfig;
